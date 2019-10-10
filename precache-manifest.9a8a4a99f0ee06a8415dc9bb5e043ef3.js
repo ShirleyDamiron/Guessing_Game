b0d4b23ff4bb4ac8e7f43818ec92d3678f347a15
Shirley Damiron <shirleydamiron@Administrators-MacBook-Pro.local>
2019-10-10 18:11:10 -0400

    Updates

---
+++ precache-manifest.9a8a4a99f0ee06a8415dc9bb5e043ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1b6ed7c356d979e721bdd779fcbcf4",
+    "url": "/Guessing_Game/index.html"
+  },
+  {
+    "revision": "4a5e505c07ae4bffd5c8",
+    "url": "/Guessing_Game/static/css/main.9a71a0cf.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/Guessing_Game/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "4a5e505c07ae4bffd5c8",
+    "url": "/Guessing_Game/static/js/main.7f1e8d20.chunk.js"
+  },
+  {
+    "revision": "1c890ff51554cf6ad5ba",
+    "url": "/Guessing_Game/static/js/runtime~main.fa76fb74.js"
+  }
+]);
